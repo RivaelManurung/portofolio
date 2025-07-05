8fc6e52b557a12b459d8ea4d1b76e00cae3af910
Kristina <135939684+RivaelManurung@users.noreply.github.com>
2025-07-06 01:40:38 +0700

    tictac

--- src/data/user_info.js
+++ src/data/user_info.js
@@ -1,209 +1,218 @@
 const info = {
-    // ============ MAIN DETAILS ============
-    main: {
-        name: "Rivael Hasiholan Manurung",
-        description: "As a back-end engineer, I focus on creating and maintaining the server-side parts of applications to ensure they work well with the front end. I work with various programming languages like Node.js, PHP, Java and Golang, and have a lot of experience using the Laravel framework. I also contribute to the whole development process of projects. In addition to back-end skills, I know front-end development with React JS and manage SQL databases effectively. I follow best practices in software development to build scalable and efficient systems, manage databases well, and perform thorough testing. My commitment to quality has led to reliable back-end solutions that improve overall system performance and provide a good user experience. Overall, I aim to connect the front and back ends smoothly, creating a cohesive and efficient application environment.",
-        role: "Backend Developer",
-        photo: "../rivael.png",
-        email: "rivaelmanurung3@gmail.com",
+  // ============ MAIN DETAILS ============
+  main: {
+    name: "Rivael Hasiholan Manurung",
+    description:
+      "As a back-end engineer, I focus on creating and maintaining the server-side parts of applications to ensure they work well with the front end. I work with various programming languages like Node.js, PHP, Java and Golang, and have a lot of experience using the Laravel framework. I also contribute to the whole development process of projects. In addition to back-end skills, I know front-end development with React JS and manage SQL databases effectively. I follow best practices in software development to build scalable and efficient systems, manage databases well, and perform thorough testing. My commitment to quality has led to reliable back-end solutions that improve overall system performance and provide a good user experience. Overall, I aim to connect the front and back ends smoothly, creating a cohesive and efficient application environment.",
+    role: "Backend Developer",
+    photo: "../rivael.png",
+    email: "rivaelmanurung3@gmail.com",
+  },
+
+  // ============ SOCIAL LINKS ============
+  socials: {
+    twitter: "https://x.com/ManroeRivael",
+    github: "https://github.com/RivaelManurung",
+    linkedin: "https://linkedin.com/in/rivaelmanurung",
+    instagram: "https://www.instagram.com/rivael_m",
+    facebook: "https://www.facebook.com/rivael.manroe/",
+  },
+
+  // ============ PROJECTS ============
+  projects: [
+    {
+      title: "Uangku",
+      description:
+        "A personal finance management web application to track income, expenses, and account balances. Features a dynamic dashboard with graphs to visualize financial summaries.",
+      technologies: "Laravel, PHP, JavaScript, Bootstrap, ApexCharts, MySQL",
+      // image: "../uangku.png",
+      github: "https://github.com/RivaelManurung/UangKu",
     },
-
-    // ============ SOCIAL LINKS ============
-    socials: {
-        twitter: "https://x.com/ManroeRivael",
-        github: "https://github.com/RivaelManurung",
-        linkedin: "https://linkedin.com/in/rivaelmanurung",
-        instagram: "https://www.instagram.com/rivael_m",
-        facebook: "https://www.facebook.com/rivael.manroe/",
+    {
+      title: "SiTani",
+      description:
+        "SiTani is a website used to borrow goods from the website by farmers. This website is developed using laravel",
+      technologies: "PHP, Laravel, JavaScript, Boostrap",
+      // image: "../pa1.png",
+      github: "https://github.com/rickyananada1/PA1-Kel2",
     },
-
-    // ============ PROJECTS ============
-    projects: [
-        {
-            title: "Uangku",
-            description: "A personal finance management web application to track income, expenses, and account balances. Features a dynamic dashboard with graphs to visualize financial summaries.",
-            technologies: "Laravel, PHP, JavaScript, Bootstrap, ApexCharts, MySQL",
-            // image: "../uangku.png",
-            github: "https://github.com/RivaelManurung/UangKu",
-        },
-        {
-            title: "SiTani",
-            description: "SiTani is a website used to borrow goods from the website by farmers. This website is developed using laravel",
-            technologies: "PHP, Laravel, JavaScript, Boostrap",
-            // image: "../pa1.png",
-            github: "https://github.com/rickyananada1/PA1-Kel2",
-        },
-        {
-            title: "DelApp",
-            description: "Developed to manage data such as bookings, overnight permits, exit permits and so on.",
-            technologies: "Flutter, dart",
-            // image: "../rivael.png",
-            github: "https://github.com/RivaelManurung/DelApp",
-        },
-        {
-            title: "The Deck",
-            description: "This application was developed for ordering food with the frontend using flutter and the backend using golang along with the admin managed via the website using laravel.",
-            technologies: "Laravel, Php, Boostrap, Flutter, Dart, JavaScript, Go, Api, Html, Css",
-            // image: "../rivael.png",
-            github: "https://github.com/danielmanalu07/PA2-Kel15",
-            // link: "https://letterdigitshape-classification.netlify.app/"
-        },
-        {
-            title: "Blog",
-            description: "This website is used to manage blogs using Laravel",
-            technologies: "Laravel, Php, Boostrap, JavaScript",
-            // image: "../rivael.png",
-            github: "https://github.com/RivaelManurung/blog",
-            // link: "https://thechatroom.netlify.app/"
-        },
-        {
-            title: "Library",
-            description: "This website is used to manage blogs using Laravel",
-            technologies: "Laravel, Php, Boostrap, JavaScript",
-            // image: "../rivael.png",
-            github: " https://github.com/RivaelManurung/Library",
-            // link: "https://thechatroom.netlify.app/"
-        },
-        {
-            title: "Absensi Kominfo",
-            description: "This website is used to manage absences using Laravel",
-            technologies: "Laravel, Php, Boostrap, JavaScript",
-            // image: "../rivael.png",
-            github: " https://github.com/danielmanalu07/Komfend-WEB-DEV",
-            // link: "https://thechatroom.netlify.app/"
-        },
-
-    ],
-
-    // ============ EDUCATION ============
-    education: [
-        {
-            school: "Insitute of Technology Del",
-            degree: "Associate Degree in Information Technology",
-            duration: "2022 - 2025",
-            image: "del.png"
-        },
-    ],
-
-    // ============ EXPERIENCE ============
-    experience: [
-        {
-            position: "Web Devlopment",
-            company: "Baparekraf Digital Talent",
-            duration: "Mei 2023 - Jul 2023",
-            image: "baparekraf.png",
-            descriptions: [
-                "learn to use kotlin programming language",
-            ]
-        },
-        {
-            position: "Flutter Mobile App Development",
-            company: "SanberCode",
-            duration: "Jul 2023",
-            image: "sanbercode.jpeg",
-            descriptions: [
-                "learn to design apps using dart",
-                "create an application using the flutter framework",
-                "learn to use firebase as authentication"
-            ]
-        },
-        {
-            position: "Software Enginger",
-            company: "Kominfo",
-            duration: "Sept 2024",
-            image: "diskominfo.png",
-            descriptions: [
-                "create an website using the laravel framework",
-                "create an webiste absensi kominfo using qrcode",
-            ]
-        },
-        {
-            position: "Web Developer",
-            company: "Infite Learning",
-            duration: "Sept 2024",
-            image: "infinitelearning.png",
-            descriptions: [
-                "learn to design website with figma",
-                "create an website using the react and express js framework",
-            ]
-        }
-
-    ],
-
-    // ============ CERTIFICATES ============
-    certificates: [
-        {
-            title: "Memulai Pemrograman dengan Kotlin",
-            description: "Provided by dicoding",
-            image: "dicoding.jpeg",
-            link: "https://www.dicoding.com/certificates/MEPJVODLQP3V",
-        },
-        {
-            title: "UI UX Design",
-            description: "Provided by Rakamin",
-            image: "rakamin.png",
-            link: "https://drive.google.com/file/d/1uxIX2pssMJJLod3aSPqFwSEkjbd7VEWy/view",
-        },
-        {
-            title: "Mahir Membuat DB Aplikasi Profesional",
-            description: "Provided by Coursera & Authorized by Google",
-            image: "codingstudio.png",
-            link: "https://codingstudio.id/certificate/75BB89ACA7-75BB4C2B51-75AF9E5EAA/"
-        },
-        {
-            title: "Mahir membuat website dengan laravel 9",
-            description: "Provided by Coursera & Authorized by Google",
-            image: "codingstudio.png",
-            link: "https://codingstudio.id/certificate/75BB89ACA7-75C105C831-75AF9E5EAA/"
-        },
-        {
-            title: "Memulai Pemrograman Dengan C",
-            description: "Provided by Dicoding",
-            image: "dicoding.jpeg",
-            link: "https://www.dicoding.com/certificates/1OP80D6JVXQK"
-        },
-        {
-            title: "Problem Solving (Basic) Certificate",
-            description: "Provided by Coursera & Authorized by IBM",
-            image: "hackerrank.png",
-            link: "https://www.hackerrank.com/certificates/c5883859b172"
-        },
-        {
-            title: "Belajar Dasar Pemrograman JavaScript",
-            description: "Provided by Dicoding",
-            image: "dicoding.jpeg",
-            link: "https://www.dicoding.com/certificates/98XWV7J6WPM3"
-        },
-        {
-            title: "Flutter Mobile App Development",
-            description: "Provided by Sanber Code",
-            image: "sanbercode.jpeg",
-            link: "https://sanbercode.com/sertifikat/generate/0188eb97-5d73-4ddb-af28-0f2a0688cde5"
-        },
-        {
-            title: "HackFest Google Developer Students Club 2024",
-            description: "Provided by Google Developer Students Club",
-            image: "gdsc.png",
-            link: "https://drive.google.com/file/d/1_O_VI6osZkAIDhlRFYO44hTwd1Z4Kq1x/view?usp=sharing"
-        },
-        {
-            title: "Structured Query Language (SQL)",
-            description: "Provided by Dicoding",
-            image: "dicoding.jpeg",
-            link: "https://www.dicoding.com/certificates/JLX1W15WGP72"
-        }
-
-
-    ],
-
-    // ============ CONTACT ============
-    contact: {
-        title: "Let's Get in Touch: Ways to Connect with Me",
-        description: "Thank you for reaching out! I appreciate your feedback, questions, and suggestions. If you have something specific you'd like to discuss, feel free to email me directly at rivaelmanurung3@gmail.com. I strive to respond to all messages within 24 hours, although it may take a bit longer during busy times. Additionally, you're welcome to connect with me on social media through the links provided below.",
+    {
+      title: "DelApp",
+      description:
+        "Developed to manage data such as bookings, overnight permits, exit permits and so on.",
+      technologies: "Flutter, dart",
+      // image: "../rivael.png",
+      github: "https://github.com/RivaelManurung/DelApp",
     },
+    {
+      title: "The Deck",
+      description:
+        "This application was developed for ordering food with the frontend using flutter and the backend using golang along with the admin managed via the website using laravel.",
+      technologies:
+        "Laravel, Php, Boostrap, Flutter, Dart, JavaScript, Go, Api, Html, Css",
+      // image: "../rivael.png",
+      github: "https://github.com/danielmanalu07/PA2-Kel15",
+      // link: "https://letterdigitshape-classification.netlify.app/"
+    },
+    {
+      title: "Blog",
+      description: "This website is used to manage blogs using Laravel",
+      technologies: "Laravel, Php, Boostrap, JavaScript",
+      // image: "../rivael.png",
+      github: "https://github.com/RivaelManurung/blog",
+      // link: "https://thechatroom.netlify.app/"
+    },
+    {
+      title: "Library",
+      description: "This website is used to manage blogs using Laravel",
+      technologies: "Laravel, Php, Boostrap, JavaScript",
+      // image: "../rivael.png",
+      github: " https://github.com/RivaelManurung/Library",
+      // link: "https://thechatroom.netlify.app/"
+    },
+    {
+      title: "Absensi Kominfo",
+      description: "This website is used to manage absences using Laravel",
+      technologies: "Laravel, Php, Boostrap, JavaScript",
+      // image: "../rivael.png",
+      github: " https://github.com/danielmanalu07/Komfend-WEB-DEV",
+      // link: "https://thechatroom.netlify.app/"
+    },
+    {
+      title: "Tic Tac Toe Game",
+      description:
+        "A classic Tic Tac Toe game built with Vue.js where two players can play in turns. It includes win detection, draw conditions, and a reset button.",
+      technologies: "Vue.js, JavaScript, HTML, CSS",
+      github: "https://github.com/RivaelManurung/tictactoe", // ganti dengan URL repo kamu kalau sudah ada
+      // link: "https://your-deploy-link.com" // opsional kalau sudah kamu deploy
+    },
+  ],
 
-    // ============ FOOTER ============
-    footer: "© 2024 Rivael Hasiholan Manurung. All Rights Reserved"
-}
+  // ============ EDUCATION ============
+  education: [
+    {
+      school: "Insitute of Technology Del",
+      degree: "Associate Degree in Information Technology",
+      duration: "2022 - 2025",
+      image: "del.png",
+    },
+  ],
 
-export default info
+  // ============ EXPERIENCE ============
+  experience: [
+    {
+      position: "Web Devlopment",
+      company: "Baparekraf Digital Talent",
+      duration: "Mei 2023 - Jul 2023",
+      image: "baparekraf.png",
+      descriptions: ["learn to use kotlin programming language"],
+    },
+    {
+      position: "Flutter Mobile App Development",
+      company: "SanberCode",
+      duration: "Jul 2023",
+      image: "sanbercode.jpeg",
+      descriptions: [
+        "learn to design apps using dart",
+        "create an application using the flutter framework",
+        "learn to use firebase as authentication",
+      ],
+    },
+    {
+      position: "Software Enginger",
+      company: "Kominfo",
+      duration: "Sept 2024",
+      image: "diskominfo.png",
+      descriptions: [
+        "create an website using the laravel framework",
+        "create an webiste absensi kominfo using qrcode",
+      ],
+    },
+    {
+      position: "Web Developer",
+      company: "Infite Learning",
+      duration: "Sept 2024",
+      image: "infinitelearning.png",
+      descriptions: [
+        "learn to design website with figma",
+        "create an website using the react and express js framework",
+      ],
+    },
+  ],
+
+  // ============ CERTIFICATES ============
+  certificates: [
+    {
+      title: "Memulai Pemrograman dengan Kotlin",
+      description: "Provided by dicoding",
+      image: "dicoding.jpeg",
+      link: "https://www.dicoding.com/certificates/MEPJVODLQP3V",
+    },
+    {
+      title: "UI UX Design",
+      description: "Provided by Rakamin",
+      image: "rakamin.png",
+      link: "https://drive.google.com/file/d/1uxIX2pssMJJLod3aSPqFwSEkjbd7VEWy/view",
+    },
+    {
+      title: "Mahir Membuat DB Aplikasi Profesional",
+      description: "Provided by Coursera & Authorized by Google",
+      image: "codingstudio.png",
+      link: "https://codingstudio.id/certificate/75BB89ACA7-75BB4C2B51-75AF9E5EAA/",
+    },
+    {
+      title: "Mahir membuat website dengan laravel 9",
+      description: "Provided by Coursera & Authorized by Google",
+      image: "codingstudio.png",
+      link: "https://codingstudio.id/certificate/75BB89ACA7-75C105C831-75AF9E5EAA/",
+    },
+    {
+      title: "Memulai Pemrograman Dengan C",
+      description: "Provided by Dicoding",
+      image: "dicoding.jpeg",
+      link: "https://www.dicoding.com/certificates/1OP80D6JVXQK",
+    },
+    {
+      title: "Problem Solving (Basic) Certificate",
+      description: "Provided by Coursera & Authorized by IBM",
+      image: "hackerrank.png",
+      link: "https://www.hackerrank.com/certificates/c5883859b172",
+    },
+    {
+      title: "Belajar Dasar Pemrograman JavaScript",
+      description: "Provided by Dicoding",
+      image: "dicoding.jpeg",
+      link: "https://www.dicoding.com/certificates/98XWV7J6WPM3",
+    },
+    {
+      title: "Flutter Mobile App Development",
+      description: "Provided by Sanber Code",
+      image: "sanbercode.jpeg",
+      link: "https://sanbercode.com/sertifikat/generate/0188eb97-5d73-4ddb-af28-0f2a0688cde5",
+    },
+    {
+      title: "HackFest Google Developer Students Club 2024",
+      description: "Provided by Google Developer Students Club",
+      image: "gdsc.png",
+      link: "https://drive.google.com/file/d/1_O_VI6osZkAIDhlRFYO44hTwd1Z4Kq1x/view?usp=sharing",
+    },
+    {
+      title: "Structured Query Language (SQL)",
+      description: "Provided by Dicoding",
+      image: "dicoding.jpeg",
+      link: "https://www.dicoding.com/certificates/JLX1W15WGP72",
+    },
+  ],
+
+  // ============ CONTACT ============
+  contact: {
+    title: "Let's Get in Touch: Ways to Connect with Me",
+    description:
+      "Thank you for reaching out! I appreciate your feedback, questions, and suggestions. If you have something specific you'd like to discuss, feel free to email me directly at rivaelmanurung3@gmail.com. I strive to respond to all messages within 24 hours, although it may take a bit longer during busy times. Additionally, you're welcome to connect with me on social media through the links provided below.",
+  },
+
+  // ============ FOOTER ============
+  footer: "© 2024 Rivael Hasiholan Manurung. All Rights Reserved",
+};
+
+export default info;
